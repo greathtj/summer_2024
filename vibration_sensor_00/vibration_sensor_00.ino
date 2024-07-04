a93d4a705f37e8e6b273e7c4d7d921af5ac8998c
Taejin Hwang <greathtj@gmail.com>
2024-07-04 15:15:04 +0900

    Update vibration_sensor_00.ino

--- vibration_sensor_00/vibration_sensor_00.ino
+++ vibration_sensor_00/vibration_sensor_00.ino
@@ -2,20 +2,11 @@
 #include "MPU6050.h"
 #include <Esp.h>
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
 MPU6050 accelgyro;
-// MPU6050 accelgyro(0x69); // <-- use for AD0 high
-// MPU6050 accelgyro(0x68, &Wire1); // <-- use for AD0 low, but 2nd Wire (TWI/I2C) object
-
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
@@ -43,18 +34,12 @@ void setup() {
 }
 
 void loop() {
-    // read raw accel/gyro measurements from device
-    // accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
-    // these methods (and a few others) are also available
     accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
 
     Serial.print(ax); Serial.print("\t");
     Serial.print(ay); Serial.print("\t");
     Serial.print(az); Serial.println("");
 
-    // blink LED to indicate activity
     blinkState = !blinkState;
     digitalWrite(LED_PIN, blinkState);
     // delay(100);
